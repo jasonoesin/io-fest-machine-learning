d1da7a6a4b752ea40db965b125fad2db1362689b
TinTinWinata <tintin6892@gmail.com>
2023-04-29 00:30:01 +0700

    fix: change to json notation

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import *
-import cv2
-from model import Model
 import sys
 
+import cv2
+from flask import *
+
+from model import Model
 
 app = Flask(__name__)
 cv2.ocl.setUseOpenCL(False)
@@ -14,30 +15,31 @@ model = Model()
 # Routes
 @app.route('/predict', methods=['POST'])
 def predict():
-    high_bp = float(request.form.get('high_bp'))
-    high_chol = float(request.form.get('high_chol'))
-    chol_check = float(request.form.get('chol_check'))
+    data = request.get_json()
+    high_bp = float(data['high_bp'])
+    high_chol = float(data['high_chol'])
+    chol_check = float(data['chol_check'])
 
-    bmi = float(request.form.get('bmi'))
-    smoker = float(request.form.get('smoker'))
-    stroke = float(request.form.get('stroke'))
+    bmi = float(data['bmi'])
+    smoker = float(data['smoker'])
+    stroke = float(data['stroke'])
 
-    heart_disease = float(request.form.get('heart_disease'))
-    phys_activity = float(request.form.get('phys_activity'))
-    fruits = float(request.form.get('fruits'))
+    heart_disease = float(data['heart_disease'])
+    phys_activity = float(data['phys_activity'])
+    fruits = float(data['fruits'])
 
-    veggies = float(request.form.get('veggies'))
-    heavy_alc = float(request.form.get('heavy_alc'))
-    health_insurance = float(request.form.get('health_insurance'))
+    veggies = float(data['veggies'])
+    heavy_alc = float(data['heavy_alc'])
+    health_insurance = float(data['health_insurance'])
 
-    no_doc_bc_cost = float(request.form.get('no_doc_bc_cost'))
-    gen_health = float(request.form.get('gen_health'))
-    mental_health = float(request.form.get('mental_health'))
+    no_doc_bc_cost = float(data['no_doc_bc_cost'])
+    gen_health = float(data['gen_health'])
+    mental_health = float(data['mental_health'])
 
-    phys_health = float(request.form.get('phys_health'))
-    diff_walk = float(request.form.get('diff_walk'))
-    sex = float(request.form.get('sex'))
-    age_category = float(request.form.get('age_category'))
+    phys_health = float(data['phys_health'])
+    diff_walk = float(data['diff_walk'])
+    sex = float(data['sex'])
+    age_category = float(data['age_category'])
 
     result = model.predict(
         high_bp, high_chol, chol_check, bmi, smoker, stroke,
